26152e2f0effb256e3e7924eb27662d308d62668
Zakariya Bazaou <z.bazaou@outlook.fr>
2020-07-04 19:46:29 +0200

    tchat fini

---
+++ js/app.js
@@ -0,0 +1,49 @@
+function getMessages(){
+    const requeteAjax = new XMLHttpRequest();
+    requeteAjax.open("GET", "zak.php");
+
+    requeteAjax.onload = function(){
+        const resultat = JSON.parse(requeteAjax.responseText);
+        
+        const html = resultat.reverse().map(function(message){
+            return `
+            <div class="message">
+            <span class="date">${message.created_at.substring(11,16)}</span>
+            <span class="author">${message.author}</span> :
+            <span class="content">${message.content}</span>
+        </div>
+            
+            `
+        }).join('');
+        const messages = document.querySelector('.messages');
+
+        messages.innerHTML= html;
+        messages.scrollTop= messages.scrollHeight;
+
+    }
+    requeteAjax.send();
+
+}
+function postMessage(event){
+    event.preventDefault();
+    const author = document.querySelector('#author');
+    const content = document.querySelector('#content');
+
+    const data = new FormData();
+    data.append('author',author.value);
+    data.append('content',content.value);
+
+    const requeteAjax = new XMLHttpRequest();
+    requeteAjax.open('POST', 'zak.php?task=write');
+    requeteAjax.onload = function(){
+        content.value ='';
+        content.focus();
+        getMessages();
+    }
+    requeteAjax.send(data);
+}
+document.querySelector('form').addEventListener('submit', postMessage);
+
+const interval= window.setInterval(getMessages, 6000);
+
+getMessages();
--- zak.php
+++ zak.php
@@ -1,3 +1,4 @@
+
 <?php
     $db = new PDO('mysql:host=localhost;dbname=chat;charset=utf8','root','',[
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
